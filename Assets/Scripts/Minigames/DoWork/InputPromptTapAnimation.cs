b25b04e01f989d1da1b91a5331088b31e0329b71
miggysy <123222299+miggysy@users.noreply.github.com>
2023-07-16 17:32:56 +0800

    Add Input Prompts

---
+++ Assets/Scripts/Minigames/DoWork/InputPromptTapAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPromptTapAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    private void PressDown()
+    {
+        animator.SetTrigger("PressDown");
+    }
+
+    private void PressUp()
+    {
+        animator.SetTrigger("PressUp");
+    }
+
+    private void OnEnable()
+    {
+        DoWork.onPressDown += PressDown;
+        DoWork.onPressUp += PressUp;
+    }
+
+    private void OnDisable()
+    {
+        DoWork.onPressDown -= PressDown;
+        DoWork.onPressUp -= PressUp;
+    }
+}
